a4fbd59e67ffdfd6b3ae28d8a3c97ef47ca60f71
ZanyeNair <zayne.nair@circuitrunners.com>
2023-10-01 19:16:00 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,14 +3,21 @@ public class Main{
     public static void main(String[] args) {
         String[] colors = {"Red", "Orange", "Yellow", "Green", "Blue", "Purple"};
         int[] size = {1, 2, 3, 4, 5, 6};
-        int[] pole1;
-        int[] pole2;
-        int[] pole3;
+        int[] pole1 = {};
+        int[] pole2 = {};
+        int[] pole3 = {};
 
         //This is a game with 3 poles. You have to sort the rings without putting a big one above a small one
         pole pole = new pole();
 
         pole.checkSize(size, colors, 2);
+        int length = size.length;
+        for (int i = length -1; i > -1; i--){
+            pole1[i] = size[i];
+            System.out.println(pole1[i]);
+        }
+
+
 
     }
 }
