4157b24b845c156e02a7c3ba1b035b56b3ae3a98
ZanyeNair <zayne.nair@circuitrunners.com>
2023-10-01 17:06:31 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 public class Main{
     public static void main(String[] args) {
         String[] cars = {"Volvo", "BMW", "Ford"};
-        
+        int length = cars.length;
+        for (int x = 0; x < length; x++){
+            System.out.println(cars[x]);
+        }
     }
 }
