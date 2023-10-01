b6dcfc30d496bb4bdfe2b22551bf565fcad01910
ZanyeNair <zayne.nair@circuitrunners.com>
2023-10-01 17:02:22 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 public class Main{
     public static void main(String[] args) {
-        int x = 1;
+        int x = 3;
+        System.out.println(x);
+        System.out.println(x);
+        System.out.println(x);
         System.out.println(x);
     }
 }
