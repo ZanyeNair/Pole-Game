eec0ade98f37857f8ac985fc99b842aa4edd432e
Zayne Nair <141592436+ZanyeNair@users.noreply.github.com>
2023-10-01 22:44:47 -0400

    Update pole.java

--- pole.java
+++ pole.java
@@ -1,51 +1,52 @@
+import java.util.ArrayList;
 public class pole{
-    public void checkSize(int[]size, String[] colors, int x){
+    public void checkSize(ArrayList<Integer> org, ArrayList<String> colors, int x){
         //x is the number ring they are checking
-        for (int i = 0; i < colors.length; i++){
+        for (int i = 0; i < colors.size(); i++){
             
-            if (size[i] == x+1){
-                System.out.print(colors[x] + " ");
+            if (org.get(i)== x+1){
+                System.out.print(colors.get(x) + " ");
             }
         }
     }
-    public void firstPole(int[] pole1, int[] size, String[] colors){
+    public void firstPole(int[] pole1, ArrayList<Integer> org, ArrayList<String> colors){
     
         for (int i = 0; i < pole1.length; i++){
-            this.checkSize(size, colors, i);
+            this.checkSize(org, colors, i);
         }
         
     }
-    public void secondPole(int[] pole2, int[] size, String[] colors){
+    public void secondPole(int[] pole2, ArrayList<Integer> org, ArrayList<String> colors){
         if (pole2.length == 0){
             System.out.println("This pole is empty");
         }
         else{
             for (int i = 0; i < pole2.length; i++){
-                this.checkSize(size, colors, i);
+                this.checkSize(org, colors, );
             }
         }
     
     }
-    public void thirdPole(int[] pole3, int[] size, String[] colors){
+    public void thirdPole(int[] pole3, ArrayList<Integer> org, ArrayList<String> colors){
         if (pole3.length == 0){
             System.out.println("This pole is empty");
         }
         else{
             for (int i = 0; i < pole3.length; i++){
-                this.checkSize(size, colors, i);
+                this.checkSize(org, colors, i);
             }
         }
         
     }
-    public void checkingAllPoles(int[] pole3, int[] size, String[] colors, int[] pole1, int[] pole2){
+    public void checkingAllPoles(ArrayList<Integer> pole3, ArrayList<Integer> org, ArrayList<String> colors, ArrayList<Integer> pole1, ArrayList<Integer> pole2){
         System.out.print("The colors on the first pole is: ");
-        this.firstPole(pole1, size, colors);
+        this.firstPole(pole1, org, colors);
         System.out.println("\n");
         System.out.print("The colors on the second pole is: ");
-        this.secondPole(pole2, size, colors);
+        this.secondPole(pole2, org, colors);
         System.out.println("\n");
         System.out.print("The colors on the third pole is: ");
-        this.thirdPole(pole3, size, colors);
+        this.thirdPole(pole3, org, colors);
     }
     public int getNumberVColor(String color){
         if (color == "red"){
@@ -70,11 +71,31 @@ public class pole{
             return 0;
         }
     }
-    public boolean movePole(int[] pole3, int[] size, String[] colors, int[] pole1, int[] pole2, String colorMove, int poleMove, boolean moveTrue){
+    public boolean movePole(int[] pole3, ArrayList<Integer> org, String[] colors, int[] pole1, int[] pole2, String colorMove, int poleMove, boolean moveTrue){
+        int numColor;
+        numColor = this.getNumberVColor(colorMove);
+        int poleStartOn;
+        if (pole1[0] == numColor){
+            poleStartOn = 1;
+        }
+        else if (pole2[0] == numColor){
+            poleStartOn = 2;
+        }
+        else if (pole3[0] == numColor){
+            poleStartOn = 3;
+        }
+        else{
+            moveTrue = false;
+            return moveTrue;
+        }
         if (poleMove == 1){
-            if(this.getNumberVColor(colorMove) == 0){
+            
+            if(numColor == 0){
                 moveTrue = false;
                 return moveTrue;
+            }
+            if(pole1[0] > numColor){
+                
             }
             return moveTrue;
         }
@@ -96,5 +117,6 @@ public class pole{
             moveTrue = false;
             return moveTrue;
         }
-    }  
+    }
+    
 }
