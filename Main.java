033f5fa35c711959284432ed13f81821b6829f23
ZanyeNair <zayne.nair@circuitrunners.com>
2023-10-01 22:43:47 -0400

    Second Time

--- Main.java
+++ Main.java
@@ -1,21 +1,46 @@
+import java.util.Scanner;
+
 public class Main{
     
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
         String[] colors = {"Red", "Orange", "Yellow", "Green", "Blue", "Purple"};
         int[] size = {1, 2, 3, 4, 5, 6};
-        int[] pole1 = {};
+        int[] pole1 = {1, 2, 3, 4, 5, 6};
         int[] pole2 = {};
         int[] pole3 = {};
 
-        //This is a game with 3 poles. You have to sort the rings without putting a big one above a small one
+        //This is a game with 3 poles. You have to sort the rings without putting a big one above a small one. The objective is 
         pole pole = new pole();
-
-        pole.checkSize(size, colors, 2);
-        int length = size.length;
-        for (int i = length -1; i > -1; i--){
-            pole1[i] = size[i];
-            System.out.println(pole1[i]);
+        System.out.print("The colors that you are starting with is: ");
+        for( int i = 0; i< size.length; i++){
+            
+            pole.checkSize(size, colors, i);
         }
+        System.out.println("\nThe order of size is red being the smallest and purple being the biggest. The objective of the game is to get all the rings on pole 3 in order.");
+
+        pole.checkingAllPoles(pole3, size, colors, pole1, pole2);
+        boolean boo = true;
+        boolean moveTrue = true;
+        while(boo == true){
+            System.out.print("Which color ring would you like moved to: ");
+            String colorMove = in.nextLine();
+            colorMove = colorMove.toLowerCase();
+            System.out.print("Which pole number would you like it to get moved to: ");
+            int poleMove = in.nextInt();
+            moveTrue = pole.movePole(pole3,size,colors,pole1, pole2,colorMove,poleMove,moveTrue);
+            if (moveTrue){
+                System.out.println("The current state of the pole is");
+                pole.checkingAllPoles(pole3, size, colors, pole1, pole2);
+            }
+            else{
+                System.out.println("This move is not legal");
+            }
+
+        }
+
+        
 
 
 
--- pole.java
+++ pole.java
@@ -2,9 +2,99 @@ public class pole{
     public void checkSize(int[]size, String[] colors, int x){
         //x is the number ring they are checking
         for (int i = 0; i < colors.length; i++){
-            if (size[i] == x){
-                System.out.println("The color is " + colors[x]);
+            
+            if (size[i] == x+1){
+                System.out.print(colors[x] + " ");
             }
         }
     }
+    public void firstPole(int[] pole1, int[] size, String[] colors){
+    
+        for (int i = 0; i < pole1.length; i++){
+            this.checkSize(size, colors, i);
+        }
+        
+    }
+    public void secondPole(int[] pole2, int[] size, String[] colors){
+        if (pole2.length == 0){
+            System.out.println("This pole is empty");
+        }
+        else{
+            for (int i = 0; i < pole2.length; i++){
+                this.checkSize(size, colors, i);
+            }
+        }
+    
+    }
+    public void thirdPole(int[] pole3, int[] size, String[] colors){
+        if (pole3.length == 0){
+            System.out.println("This pole is empty");
+        }
+        else{
+            for (int i = 0; i < pole3.length; i++){
+                this.checkSize(size, colors, i);
+            }
+        }
+        
+    }
+    public void checkingAllPoles(int[] pole3, int[] size, String[] colors, int[] pole1, int[] pole2){
+        System.out.print("The colors on the first pole is: ");
+        this.firstPole(pole1, size, colors);
+        System.out.println("\n");
+        System.out.print("The colors on the second pole is: ");
+        this.secondPole(pole2, size, colors);
+        System.out.println("\n");
+        System.out.print("The colors on the third pole is: ");
+        this.thirdPole(pole3, size, colors);
+    }
+    public int getNumberVColor(String color){
+        if (color == "red"){
+            return 1;
+        }
+        else if(color == "orange"){
+            return 2;
+        }
+        else if(color == "yellow"){
+            return 2;
+        }
+        else if(color == "green"){
+            return 2;
+        }
+        else if(color == "blue"){
+            return 2;
+        }
+        else if(color == "purple"){
+            return 2;
+        }
+        else{
+            return 0;
+        }
+    }
+    public boolean movePole(int[] pole3, int[] size, String[] colors, int[] pole1, int[] pole2, String colorMove, int poleMove, boolean moveTrue){
+        if (poleMove == 1){
+            if(this.getNumberVColor(colorMove) == 0){
+                moveTrue = false;
+                return moveTrue;
+            }
+            return moveTrue;
+        }
+        else if(poleMove == 2){
+            if(this.getNumberVColor(colorMove) == 0){
+                moveTrue = false;
+                return moveTrue;
+            }
+            return moveTrue;
+        }
+        else if(poleMove == 3){
+            if(this.getNumberVColor(colorMove) == 0){
+                moveTrue = false;
+                return moveTrue;
+            }
+            return moveTrue;
+        }
+        else{
+            moveTrue = false;
+            return moveTrue;
+        }
+    }  
 }
