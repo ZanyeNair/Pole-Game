066924a766bc1016ba4e05511ab8afbd7e096128
ZanyeNair <zayne.nair@circuitrunners.com>
2023-12-18 18:03:21 -0500

    test

--- Main.java
+++ Main.java
@@ -10,16 +10,56 @@ public class Main{
     }
 }
 
-class Methods{
-    
-    public int[] dup(int[] arr){
-        int size = arr.size();
-        int[] dup = new int[(2*size)];
-        for (int i = 0; i < size; i++){
-            dup[i*2] = arr[i];
-            dup[i*2+1] = arr[i];
-        }
-        return dup;
+public class Methods{
+
+    int[] arr;
+
+    public Methods(int[] s){
+        arr =s;
     }
 
+    public int getMin(){
+        int min = arr[0];
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i]<min){
+                min = arr[i];
+            }
+        }
+        return min;
+       }
+
+    public boolean isIncreasing(){
+        for(int i = 1; i<arr.length; i++){
+            if(arr[i]<arr[i-1]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public int[] removeDuplicate(){
+        for(int i = 1; i<arr.length; i++){
+            if(arr[i] == arr[i-1]){
+                if(!this.isIncreasing()){
+                    arr[i] = 0;
+                    arr[i-1] = 0;
+                }
+                else{
+                    if(i == arr.length-1){
+                        arr[i] = 0;
+                        arr[i-1] = 0;
+                    }
+                    else{
+                        for(int j = i+1; j<arr.length; j++){
+                            arr[j-2] = arr[j];
+                        }
+                        arr[arr.length-2] = 0;
+                        arr[arr.length-1] = 0;
+                    }
+                }
+            }
+        }
+        return arr;
+    }
+    
 }
