9c1d0987525b4a5ab1aaa6da625227131716093a
ZanyeNair <zayne.nair@circuitrunners.com>
2023-12-18 15:22:02 -0500

    w

--- Main.java
+++ Main.java
@@ -1,8 +1,21 @@
 public class Main{
     public static void main(String[] args) {
-        System.out.println("yes");
-        for(int x = 0; x <=10; x++){
-            System.out.println(x);
-        }
+        new int[] arr = {1,2,3};
+        Methods n = new Methods;
+        System.out.println(n.dup(arr));
     }
+}
+
+class Methods{
+    
+    public int[] dup(int[] arr){
+        int size = arr.size();
+        int[] dup = new int[(2*size)];
+        for (int i = 0; i < size; i++){
+            dup[i*2] = arr[i];
+            dup[i*2+1] = arr[i];
+        }
+        return dup;
+    }
+
 }
